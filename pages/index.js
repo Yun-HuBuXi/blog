13c06008d595d0f1316af8defeaf83cd626a1c6a
FengXu <1652158793@qq.com>
2021-12-11 19:03:57 +0800

    add menu

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,19 @@ import Head from "next/head"
 import Layout from "../components/layout"
 import Intro from "../components/intro"
 import Content from "../components/content"
+import Menu1 from "../components/menu"
 
 export default function Home() {
   return (
-    <Layout>
+    <div className="body">
+      <Menu1 className="menu"></Menu1>
+      <Layout className="layout">
         <Head>
-            <title>Welcome to My Blog</title>
+          <title>Welcome to My Blog</title>
         </Head>
         <Intro></Intro>
         <Content></Content>
-    </Layout>
+      </Layout>
+    </div>
   )
 }
